88a3628dc95054bf47f787244d9b68d1339095bc
Rikucode <pavlyu4enko.a@gmail.com>
2022-07-02 23:41:49 +1000

    Update Calculator.cpp
    добавлен бинарный минус

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stack>
+#include <cmath>
 
 using namespace std;
 
@@ -65,6 +66,19 @@ bool maths(stack <Lexema> & stack_number, stack <Lexema> & stack_operation){
             stack_number.push(lexema);
             break;
 
+        case '%' :
+            buz = stack_number.top().value;
+            stack_number.pop();
+            if (foo == 0) {
+                cerr << "Error: divided by zero!";
+                return false;
+            }
+            bar = buz - (floor(buz / foo) * foo);
+            lexema.value = bar;
+            lexema.type = '0';
+            stack_number.push(lexema);
+            break;
+
         default :
             cerr << "Unknown operation!";
             return false;
@@ -75,19 +89,21 @@ bool maths(stack <Lexema> & stack_number, stack <Lexema> & stack_operation){
 
 int main(){
     char cur_char;
+    bool first_minus_flag = true;
     stack <Lexema> stack_number;
     stack <Lexema> stack_operation;
     Lexema lexema;
     while(true) {
         cur_char = cin.peek();
         if (cur_char == '\n') break;
-        if (cur_char >= '0' && cur_char <= '9') {
+        if (cur_char >= '0' && cur_char <= '9' || first_minus_flag && cur_char == '-') {
+            first_minus_flag = false;
             lexema.type = '0';
             cin >> lexema.value;
             stack_number.push(lexema);
             continue;
         }
-        if (cur_char == '+' || cur_char == '-' || cur_char == '*' || cur_char == '/') {
+        if (cur_char == '+' || cur_char == '-' || cur_char == '*' || cur_char == '/' || cur_char == '%') {
             if (stack_operation.empty() || getPrioritet(cur_char) > getPrioritet(stack_operation.top().type)) {
                 lexema.type = cur_char;
                 lexema.value = 0;
